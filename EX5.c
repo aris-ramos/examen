48dca38c9d4f80fb9ce65f4c4dd98cfc606a1830
aris-ramos <82551184+aris-ramos@users.noreply.github.com>
2021-05-12 21:19:22 -0500

    Add files via upload

---
+++ EX5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () 
+{
+     /* Ramos Morales Arisbeth
+    Equipo: los burros dorados del 17
+    */
+   float numerodepersonas, numerodealimentos, porciones;
+   printf ("\n\n Programa 1");
+   printf ("\n\n Dame el numero de personas");
+   scanf ("%f",&numerodepersonas);
+   printf ("\n\n Dame el numero de alimentos disponibles");
+   scanf ("%f",&numerodealimentos);
+   porciones=numerodealimentos/numerodepersonas;
+   printf ("\n\n La porcion para dividir equitativamente a cada persona es %f",porciones);
+    return 0;
+}
+
+   
